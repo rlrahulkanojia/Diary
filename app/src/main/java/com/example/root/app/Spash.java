b55c16e702c647dc1fd21ab1dcccf948ace314aa
rlrahulkanojia <rlrahulkanojia@gmail.com>
2017-07-05 21:51:45 +0530

    "Check 1"

--- app/src/main/java/com/example/root/app/Spash.java
+++ app/src/main/java/com/example/root/app/Spash.java
@@ -13,7 +13,7 @@ public class Spash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spash);//chexck
+        setContentView(R.layout.activity_spash);//chexckhg
         pgBar=(ProgressBar)findViewById(R.id.PBar);
         Thread myThread=new Thread(){
         @Override
